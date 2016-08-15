6b36f03373915d00dda695173218ba5bd10c4842
IKANE <kaneelhadji2610@yahoo.fr>
2016-08-16 00:07:31 +0200

    fdgdf

--- src/main/resources/static/app/main/main.controller.js
+++ src/main/resources/static/app/main/main.controller.js
@@ -1,20 +1,23 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name demoAngularGeneratedApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the demoAngularGeneratedApp
- */
-angular.module('demoAngularGeneratedApp')
-  .controller('MainController', MainController);
-
-function MainController () {
-    var vm = this;
-	vm.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-}
+(function() {
+	'use strict';
+	
+	/**
+	 * @ngdoc function
+	 * @name demoAngularGeneratedApp.controller:MainCtrl
+	 * @description
+	 * # MainCtrl
+	 * Controller of the demoAngularGeneratedApp
+	 */
+	angular.module('demoAngularGeneratedApp')
+	  .controller('MainController', MainController);
+	
+	function MainController () {
+	    var vm = this;
+		vm.awesomeThings = [
+	      'HTML5 Boilerplate',
+	      'AngularJS',
+	      'Bootstrap',
+	      'Karma'
+	    ];
+	}
+})();
