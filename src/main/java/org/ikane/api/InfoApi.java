21d8940da35597cc66c289a5613134185cae578a
IKANE <kaneelhadji2610@yahoo.fr>
2017-04-23 21:54:45 +0200

    update

--- src/main/java/org/ikane/api/InfoApi.java
+++ src/main/java/org/ikane/api/InfoApi.java
@@ -3,6 +3,9 @@ package org.ikane.api;
 import org.ikane.dto.FormDataDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +17,19 @@ public class InfoApi {
 	
 	final static Logger logger = LoggerFactory.getLogger(InfoApi.class);
 	
+	@Autowired
+	private Environment env;
+	
 	@RequestMapping(value="/info", method=RequestMethod.GET)
 	public String home() {
 		logger.debug("Home");
 		
 		return "Hello";
+	}
+	
+	@GetMapping(path="/appVersion") 
+	public String getAppVersion() {
+		return env.getProperty("info.appVersion");
 	}
 
 	@RequestMapping(value="/info", method=RequestMethod.POST)
